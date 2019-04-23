e05734bd50d3a01347d92bad44aa7d7ee658da61
aditya agarwal <adityaa16agarwal@gmail.com>
2019-04-24 01:32:47 +0530

    1 commit

---
+++ sourcecode.cpp
@@ -0,0 +1,300 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#define rw 80
+#define cl 50
+FILE*fp,*fp1,*f1,*f2;
+int s,z;
+char fn1[]="opd12.patient";
+char fn2[]="oopd12.patient";
+struct hospital{
+		char name[20],address[20],ch;
+		int age,roomno,sn;
+		char disease[30],department[20],date[15];
+		char recommendation[50],category[30];
+		char test[15][20];
+		float testfee[15];
+		float totalfee;
+		float balance;
+		}p,q;
+char string[20];
+typedef struct hospital alka;
+void newrecord(int l);
+void print();
+void displaytest();
+void mainscreen();
+void newrecord1();
+void displaydepartment();
+void edit1();
+void editrecord();
+void switch1();
+void main()
+{
+	int a,i,n,y;
+	char c,d;
+	char date1[15],date2[15],string1[20];
+	unsigned int tsz;
+
+	clrscr();
+	mainscreen();
+	label3:
+	textcolor(3);
+	gotoxy(23,15);
+	cprintf("Enter today's Date(yyyy/mm/dd)");
+	fflush(stdin);gotoxy(28,19);
+	scanf("%[^\n]",date2);
+	if((date2[4]!='/')||(date2[7]!='/')||(date2[5]>'3')||(date2[8]>'3'))
+	{
+		clrscr();
+		mainscreen();
+		gotoxy(23,13);textcolor(4+128);
+		cprintf("Wrong Entry");
+		goto label3;
+	}
+	clrscr();
+	mainscreen();
+	label1:
+
+	textcolor(15);
+	lowvideo();gotoxy(22,15);textcolor(14);
+	cprintf("Enter the corresponding no");gotoxy(22,19);textcolor(10);
+	cprintf("1.Add new patient record");gotoxy(22,21);
+	cprintf("2.Search  or edit record");gotoxy(22,23);
+	cprintf("3.Know the records of patients");gotoxy(22,25);
+	cprintf("4.Delete the records");gotoxy(22,27);
+	cprintf("5.Exit from the program");gotoxy(25,30);
+	fflush(stdin);
+	scanf("%c",&d);
+	switch(d)
+	{
+		case '1':
+		      {
+
+			{
+				if((fp=fopen(fn1,"rb"))==NULL)
+				s=1;
+				else
+				{
+				while(fread(&p,sizeof(alka),1,fp))
+				s=1+p.sn;
+				}
+				fclose(fp);
+			}
+			clrscr();
+			mainscreen();
+			label:
+			gotoxy(22,19);textcolor(7);
+			cprintf("Enter `o' for O.P.D. & `e'for Emergency");
+			gotoxy(35,21);
+			fflush(stdin);
+			scanf("%c",&c);
+			if(c=='o')
+			{
+				clrscr();
+				mainscreen();
+				textcolor(11);gotoxy(23,11);
+				cprintf("ADDING NEW O.P.D.PATIENT RECORD");textcolor(15);
+				gotoxy(21,12);
+				cprintf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+				newrecord(s);
+				newrecord1();
+
+				if((fp=fopen(fn1,"ab+"))==NULL)
+				{
+					printf("Cannot open the file f1");
+					getch();
+					exit(1);
+				}
+				for(a=0;a<15;a++)
+				p.testfee[a]=0;
+				strcpy(&p.test[1][0],"0");
+				p.totalfee=0;p.balance=0;
+				strcpy(&p.test[0][0],"O.P.D. charge");
+				p.testfee[0]=200;
+
+				p.totalfee=200;
+				strcpy(p.category,"O.P.D.Patient");
+				p.balance=200;
+				strcpy(p.recommendation,"Admitted to O.P.D.");
+				strcpy(p.date,date2);
+				fwrite(&p,sizeof(p),1,fp);
+				fclose(fp);
+				if((fp=fopen(fn2,"ab+"))==NULL)
+				{
+						printf("Cannot open the file f1");
+						getch();
+						exit(1);
+				}
+				fwrite(&p,sizeof(p),1,fp);
+				fclose(fp);
+
+
+			}
+			else if(c=='e')
+			       {
+				 clrscr();
+				 mainscreen();
+				 textcolor(11);
+				 gotoxy(23,11);
+				 cprintf("ADDING NEW EMERGENCY PATIENT RECORD");
+				 textcolor(15);
+				 gotoxy(23,12);
+				 cprintf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+				 newrecord(s);
+				 newrecord1();
+				 if((fp=fopen(fn1,"ab+"))==NULL)
+					{
+						printf("Cannot open the file f1");
+						getch();
+						exit(1);
+					}
+					for(a=0;a<15;a++)
+					p.testfee[a]=0;
+					strcpy(&p.test[1][0],"0");
+					p.totalfee=0;p.balance=0;
+
+			       p.totalfee=250;
+			       strcpy(p.date,date2);
+			       strcpy(&p.test[0][0],"Emergency Charge");
+			       strcpy(p.category,"Emergency Patient");
+			       strcpy(p.recommendation,"Admitted to Emergency");
+			       p.testfee[0]=250;
+			       p.balance=250;
+			       fwrite(&p,sizeof(p),1,fp);
+			       fclose(fp);
+			      if((fp=fopen(fn2,"ab+"))==NULL)
+				{
+						printf("Cannot open the file f1");
+						getch();
+						exit(1);
+				}
+				fwrite(&p,sizeof(p),1,fp);
+				fclose(fp);
+
+			       }
+			else
+			{
+				clrscr();
+				mainscreen();
+				gotoxy(22,15);textcolor(128+5);
+				cprintf("Wrong choice");
+				textcolor(15);
+				goto label;
+			}
+			break;
+		      }
+		case '2':
+		      {
+		       clrscr();
+		       mainscreen();
+		       editrecord();
+		       break;
+		      }
+		case '5':
+		      {
+			clrscr();
+			mainscreen();
+			textcolor(14); gotoxy(30,24);
+			cprintf("THANK U");gotoxy(30,26);
+		       //	cprintf("SAVING UR SETTINGS"); gotoxy(30,28);
+			cprintf("BYE...........");
+			getch();
+			exit(1);
+			break;
+			}
+		case '4':
+			{
+				if((fp=fopen(fn1,"rb"))==NULL)
+				{
+					printf("cannot open the file");
+					getch();
+					exit(1);
+				}
+
+				if((f1=fopen("delete","wb"))==NULL)
+				{
+					printf("cannot open the file");
+					getch();
+					exit(1);
+				}
+
+				clrscr();
+				mainscreen();
+				gotoxy(30,25);
+				cprintf("Enter the patient no");
+				gotoxy(40,27);
+			       fflush(stdin);
+				scanf("%d",&y);
+				while(fread(&p,sizeof(alka),1,fp))
+					if(p.sn!=y)
+					fwrite(&p,sizeof(alka),1,f1);
+				clrscr();
+				mainscreen();
+				fseek(fp,(y-1)*sizeof(alka),SEEK_SET);
+				fread(&p,sizeof(alka),1,fp);
+				print();
+				edit1();
+				gotoxy(25,46);
+				cprintf("Press `ENTER' to delete this record");
+				getch();
+				fclose(fp);
+				fclose(f1);
+				remove(fn1);
+				rename("delete",fn1);
+
+				if((fp=fopen(fn2,"rb"))==NULL)
+				{
+					printf("cannot open the file");
+					getch();
+					exit(1);
+				}
+
+				if((f1=fopen("delete","wb"))==NULL)
+				{
+					printf("cannot open the file");
+					getch();
+					exit(1);
+				}
+				while(fread(&p,sizeof(alka),1,fp))
+					if(p.sn!=y)
+					fwrite(&p,sizeof(alka),1,f1);
+
+
+
+				fclose(fp);
+				fclose(f1);
+				remove(fn2);
+				rename("delete",fn2);
+				clrscr();
+				mainscreen();
+				gotoxy(25,25);textcolor(3);
+				cprintf("Record succesfully Deleted");
+				getch();
+				gotoxy(37,30);
+				break;
+			}
+
+
+		case '3':
+			{       label6:
+				clrscr();
+				mainscreen();
+				gotoxy(22,15);textcolor(12);
+				cprintf("Enter the corresponding no");gotoxy(22,19);textcolor(3);
+				cprintf("1.Records of patients in alphabatecal order");gotoxy(22,21);
+				cprintf("2.Records of Emergency patients");gotoxy(22,23);
+				cprintf("3.Records of O.P.D. patients");gotoxy(22,25);
+				cprintf("4.Recordsin paricular date");gotoxy(22,27);
+				cprintf("5.Return to main menu");gotoxy(25,30);
+				fflush(stdin);
+				scanf("%c",&d);
+				switch(d)
+				{
+				case '1':
+					{
+					 if((fp=fopen(fn2,"rb+"))==NULL)
+					      {
+						printf("Cannot open the file");
+						getch();
+						exit(1);
